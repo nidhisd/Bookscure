16751253a63c76e739e8a950ddf7fe8b806881db
Nidhi <nidhidhammani@gmail.com>
2016-12-06 15:01:36 -0800

    Initial

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package nimo.bookscure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+    }
+}
