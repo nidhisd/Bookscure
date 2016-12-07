0984b3f06aeb10cdfe4c8ab6251339ff0022f7d2
Nidhi <nidhidhammani@gmail.com>
2016-12-06 20:03:03 -0800

    Initial

--- MainActivity.java
+++ MainActivity.java
@@ -2,14 +2,56 @@ package nimo.bookscure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import android.content.*;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    CallbackManager callbackManager;
+    TextView textView;
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        textView = (TextView) findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        String DisplayLogon = loginResult.getAccessToken().getUserId() + " Logged in!!! Yayyyy!!";
+                        textView.setText(DisplayLogon);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        textView.setText("Logged Failed :(!!");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        textView.setText("Cannot Login");
+                    }
+        });
+
+
+
     }
+
 }
